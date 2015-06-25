f14082a2f9b625235a24e1c0f840c9bc40e4c8d8
zoubin <zoubin04@gmail.com>
2015-06-25 09:51:50 +0800

    readme

--- example/xbind.js
+++ example/xbind.js
@@ -13,45 +13,54 @@ console.log(
     '`arrayify` just make an array from arguments',
     arrayify(0,1,2,3)
 )
+// `arrayify` just make an array from arguments [ 0, 1, 2, 3  ]
 
 console.log(
     'use the first argument',
     xbind(arrayify).xargs(0)(0,1,2,3)
 )
+// use the first argument [ 0  ]
 
 console.log(
     'use the first argument',
     xbind(arrayify).first()(0,1,2,3)
 )
+// use the first argument [ 0  ]
 
 console.log(
     'use the last argument',
     xbind(arrayify).last()(0,1,2,3)
 )
+// use the last argument [ 3  ]
 
 console.log(
     'use the first and third arguments',
     xbind(arrayify).xargs(0,2)(0,1,2,3),
     xbind(arrayify).xargs([0,2])(0,1,2,3)
 )
+// use the first and third arguments [ 0, 2  ] [ 0, 2  ]
 
 console.log(
     'append arguments 4,5',
     xbind(arrayify).push(4,5)(0,1,2,3)
 )
+// append arguments 4,5 [ 0, 1, 2, 3, 4, 5  ]
 
 console.log(
     'prepend arguments 4,5',
     xbind(arrayify).unshift(4,5)(0,1,2,3)
 )
+// prepend arguments 4,5 [ 4, 5, 0, 1, 2, 3  ]
 
 console.log(
     'slice arguments 1,3',
     xbind(arrayify).slice(1,3)(0,1,2,3)
 )
+// slice arguments 1,3 [ 1, 2  ]
 
 console.log(
     'filter arguments',
     xbind(arrayify).xargs(1,5).filter(Boolean)(0,1,2,3)
 )
+// filter arguments [ 1  ]
 
